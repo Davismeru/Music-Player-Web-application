0599fb4176d7a9a963473e7cd98eb177a520a521
Davismeru <davismeru911@gmail.com>
2022-07-17 14:13:02 +0300

    transferred all music data from main.js to allMusic.js
    created the allMusic.js file

---
+++ js/allMusic.js
@@ -0,0 +1,86 @@
+// all songs details array (Json data)
+const allMusic = [
+    {
+        src: "music/yt5s.com - Mayorkun - Mama (Official Video) (128 kbps).mp3",
+        artist: "mayorkun",
+        song: "mama"
+    },
+
+    {
+        src: "music/yt5s.com - Brandy Maina - ( ACOUSTIC ) KUBALI X DANGER DINJI (128 kbps).mp3",
+        artist: "Brandy maina X Danger Dinji",
+        song: "Kubali"
+    },
+
+    {
+        src: "music/yt5s.com - Bruno Mars - The Lazy Song (Official Music Video) (128 kbps).mp3",
+        artist: "Bruno Mars",
+        song: "The lazy song"
+    },
+
+    {
+        src: "music/yt5s.com - CKay - Emiliana [Official Music Video] (128 kbps).mp3",
+        artist: "CKay",
+        song: "Emiliana"
+    },
+
+    {
+        src: "music/yt5s.com - Davido - FIA (Official Video) (128 kbps).mp3",
+        artist: "Davido",
+        song: "FIA"
+    },
+
+    {
+        src: "music/yt5s.com - Le Band x Suzziah - Number 1 (official video)[SMS SKIZA 9046701 to 811] (128 kbps).mp3",
+        artist: "Le Band x Suzziah",
+        song: "Number 1"
+    },
+
+    {
+        src: "music/yt5s.com - MONALISA  - LOJAY X SARZ X CHRIS BROWN (128 kbps).mp3",
+        artist: "LOJAY X SARZ X CHRIS BROWNn",
+        song: "MONALISA"
+    },
+
+    {
+        src: "music/yt5s.com - Nviiri the Storyteller - Niko Sawa ft Bien (Official Music Video) SMS [Skiza 5802169] to 811 (128 kbps).mp3",
+        artist: "mNviiri the Storyteller ft Bien",
+        song: "Niko Sawa"
+    },
+
+    {
+        src: "music/yt5s.com - Olakira - Maserati Remix [Official Video] Ft. Davido (128 kbps).mp3",
+        artist: "Olakira Ft. Davido",
+        song: "Maserati Remix"
+    },
+
+    {
+        src: "music/yt5s.com - Pheelz - Finesse ft BUJU - (Lyric Video) (128 kbps).mp3",
+        artist: "Finesse",
+        song: "Pheelz ft BUJU"
+    },
+
+    {
+        src: "music/yt5s.com - Rema - Calm Down (Official Music Video) (128 kbps).mp3",
+        artist: "Rema",
+        song: "Calm Down"
+    },
+
+    {
+        src: "music/yt5s.com - Samthing Soweto x Mzansi Youth Choir -  The Danko! Medley (128 kbps).mp3",
+        artist: "Samthing Soweto x Mzansi Youth Choir",
+        song: "The Danko!"
+    },
+
+    {
+        src: "music/yt5s.com - Sauti Sol - Tujiangalie ft Nyashinski (Official Lyric Video) SMS [Skiza 1051931] to 811 (128 kbps).mp3",
+        artist: "Sauti Sol ft Nyashinski ",
+        song: "Tujiangalie"
+    },
+
+    {
+        src: "music/Jamhuri Jam Sessions SWIGA - ROSELLA (Cover).mp3",
+        artist: "Jamhuri Jam Sessions SWIGA",
+        song: "ROSELLA (Cover)"
+    }
+]
